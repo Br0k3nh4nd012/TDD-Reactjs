55e45e9d34c80e5280227ef7e128f34a87c49967
Br0k3nh4nd012 <gokulakrishnan312@gmail.com>
2020-10-10 19:44:22 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import App from './App'
+
+ afterEach(cleanup)
+ 
+ test('should generate a snapshot for app component', () => {
+    const { asFragment } = render(<App />)
+    
+    expect(asFragment(<App />)).toMatchSnapshot()
+   })
